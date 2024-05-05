f2d6a8a8cc8a3889b688f0d948c4d71f07f5f115
ipfpv <167916397+ipfpv@users.noreply.github.com>
2024-05-05 19:47:36 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,13 @@
+my_list = ['lemon', 'lime', 'tangerine', 'plum', 'melon']
+print(my_list)
+print(my_list[0])
+print(my_list[4])
+print(my_list[2:5])
+my_list[4] = 'orange'
+print(my_list)
+
+my_dict = {'lemon' : 'лимон', 'lime' : 'лайм', 'tangerine' : 'мандарин', 'plum' : 'слива', 'melon' : 'дыня'}
+print(my_dict)
+print(my_dict['lemon'])
+my_dict['peach'] = 'персик'
+print(my_dict)
